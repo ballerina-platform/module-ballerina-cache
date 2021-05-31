c9a8594b58033762b5a04aed599864b9c2f76936
Kalaiyarasi <ashakalai31@gmail.com>
2021-05-31 12:51:19 +0530

    Remove unused import

--- cache-native/src/main/java/org/ballerinalang/stdlib/cache/nativeimpl/concurrentlinkedhashmap/ConcurrentLinkedHashMap.java
+++ cache-native/src/main/java/org/ballerinalang/stdlib/cache/nativeimpl/concurrentlinkedhashmap/ConcurrentLinkedHashMap.java
@@ -23,7 +23,6 @@ import java.io.Serializable;
 import java.util.AbstractMap;
 import java.util.AbstractSet;
 import java.util.Collection;
-import java.util.HashMap;
 import java.util.Iterator;
 import java.util.List;
 import java.util.Map;
